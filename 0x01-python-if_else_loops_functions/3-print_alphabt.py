20e462cce788cd10d123dc59c87a2bc957e50cf6
zemichaell <zemichael2014@gmail.com>
2022-06-29 13:03:53 -0700

    Write a program that prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord("a"), ord("z") + 1):
-    if chr(a) is not "q" and chr(a) is not "e":
+    if chr(a) != "q" and chr(a) != "e":
         print("{:s}".format(chr(a)), end="")
