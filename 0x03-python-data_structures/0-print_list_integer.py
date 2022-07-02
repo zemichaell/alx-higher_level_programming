0ca146bc0823fac16bb273d9ce17fdd13e4fcbb5
zemichaell <zemichael2014@gmail.com>
2022-07-02 04:07:13 -0700

    print all integer of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
         for i in my_list:
-<<<<<<< HEAD
             print("{:d}".format(i))
-=======
-                print("{:d}".format(i))
->>>>>>> 284808732282c6e6f87458d2471ee02c0beab8c5
